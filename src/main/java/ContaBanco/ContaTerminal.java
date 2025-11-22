86e21167306601200929196384e5f339fafc6c62
mateusfurt <mateus2013furt@gmail.com>
2025-11-22 17:20:43 -0300

    first commit

--- src/main/java/ContaBanco/ContaTerminal.java
+++ src/main/java/ContaBanco/ContaTerminal.java
@@ -4,6 +4,8 @@
 
 package ContaBanco;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mateus
@@ -11,6 +13,16 @@ package ContaBanco;
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("digite o numero de conta:");
+        int numeroConta = scan.nextInt();
+        System.out.println("digite o numero de agencia:");
+        int numeroagencia = scan.nextInt();
+        System.out.println("digite o nome:");
+        String nome = scan.next();
+        System.out.println("digite o saldo:");
+        int saldo = scan.nextInt();
+
+        System.out.print("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+numeroagencia+", conta "+numeroConta+" e seu saldo "+saldo+" já está disponível para saque");
     }
 }
